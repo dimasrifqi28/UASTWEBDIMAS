46d0fb28ec34b6fc7c2d9d72f54ce60d9e96403a
dimasrifqi28 <dimasrifqi28@gmail.com>
2021-12-15 01:37:39 +0700

    api config

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'get from themoviedb.org',
+    apiKey: '06e8c5b4fe76ffaa0b1fd0011c589c9e',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
